548eda7e0569e749a1ba4d4c8247d1302f987bec
mattsterchief <47911061+mattsterchief@users.noreply.github.com>
2023-06-02 17:49:36 -0400

    Duplicate Checking
    Added logic to check if the contact being added shares an email OR phone number. Returns {"error": "Duplicate Contact"}

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -15,12 +15,27 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Contacts (firstName,lastName,email,phone,userId) VALUES(?,?,?,?,?)");
-		$stmt->bind_param("sssss", $firstName, $lastName, $email, $phone, $userId);
-		$stmt->execute();
+		// Check for duplicate email/phone
+		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE userId = ? AND (email = ? OR phone = ?)");
+        $stmt->bind_param("sss", $userId, $email, $phone);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $isDuplicateContact = mysqli_num_rows($result) > 0;
+
+		if (!$isDuplicateContact)
+		{
+			$stmt = $conn->prepare("INSERT into Contacts (firstName,lastName,email,phone,userId) VALUES(?,?,?,?,?)");
+			$stmt->bind_param("sssss", $firstName, $lastName, $email, $phone, $userId);
+			$stmt->execute();
+			returnWithError("");
+		}
+		else
+		{
+			returnWithError("Duplicate Contact");
+		}
+
 		$stmt->close();
 		$conn->close();
-		returnWithError("");
 	}
 
     function getRequestInfo()
